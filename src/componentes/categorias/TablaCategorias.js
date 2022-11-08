f023a627102e6ba0810234ccb1404f6d4526e347
flopez <flopez@patprimo.com.co>
2022-11-07 19:33:53 -0500

    Se hacen ajusten al formulario de categorias

--- src/componentes/categorias/TablaCategorias.js
+++ src/componentes/categorias/TablaCategorias.js
@@ -1,8 +1,39 @@
+import Estados from "../../enums/Estados";
 import categoriaServicios from "../../servicios/categoriaServicios";
+import { useEffect, useState } from "react";
 
 const TablaCategorias = () => {
 
-    const listaCategorias = categoriaServicios.obtenerCategorias();
+    const [listaCategorias, setListaCategorias] = useState([]);
+    const [estado, setEstado] = useState(Estados.CARGANDO);
+    const [criterio, setCriterio] = useState("");
+
+    const cargarPagina = async () => {
+
+        try {
+            setEstado(Estados.CARGANDO);
+            const respuesta = await categoriaServicios.obtenerCategorias();
+            console.log(respuesta);
+            setListaCategorias(respuesta.data);
+            if (respuesta.data.length === 0) {
+                setEstado(Estados.VACIO);
+                console.log('esta vacio');
+            }
+            else {
+                setEstado(Estados.OK);
+                console.log('esta vacioo');
+            }
+        } catch (error) {
+
+            setEstado(Estados.ERROR);
+            console.log('esta error');
+        }
+    }
+    useEffect(() => {
+        cargarPagina();
+    }, []);
+
+
 
     // function ConfirmarDelete(){
     //     var respuesta =confirm("Estas seguro que deseas eliminar la categoria");
@@ -13,6 +44,29 @@ const TablaCategorias = () => {
     //         return false;
     //     }
     // }
+
+    const buscarCategoria = async (event) => {
+        event.preventDefault();
+
+        try {
+            const respuesta = await categoriaServicios.buscarCategoriasPorCriterios(criterio);
+            console.log(respuesta);
+            setListaCategorias(respuesta.data);
+            if (respuesta.data.length === 0) {
+                setEstado(Estados.VACIO);
+            }
+            else {
+                setEstado(Estados.OK);
+            }
+        } catch (error) {
+            setEstado(Estados.ERROR);
+        }
+    }
+
+    const cambiarCriterio = (event) => {
+        setCriterio(event.target.value);
+    }
+
     return (
         <div className="container">
             <h3> Lista de Categorias</h3>
@@ -20,59 +74,58 @@ const TablaCategorias = () => {
             <br></br>
             <br></br>
             <br></br>
-            {/* <table className="table table-sm">
-                <thead>
-                    <tr>
-                        <th>Nombre</th>
-                        <th>Descripcion</th>
-                        <th>Estado</th>
-                        <th>Imagen</th>
-                        <th>Acciones</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        listaCategorias.map((categoria) => (
-                            <tr>
-                                <td>{categoria.nombre}</td>
-                                <td>{categoria.descripcion}</td>
-                                <td>{categoria.estado ? "SI" : "NO"}</td>
-                                <td>{categoria.imagen}</td>
-                                <td>
-                                    <button className="btn btn-primary btn-sm me">Editar</button>
-                                    <button className="btn btn-danger btn-sm">Eliminar</button>
-                                </td>
-                            </tr>
+            <form action="">
+                <input type="text" onChange={cambiarCriterio} value={criterio} id="criterio" name="criterio" placeholder="Buscar por"></input>
+                <button className="btn btn-sm btn-primary" onClick={buscarCategoria}> Buscar</button>
+            </form>
 
-                        ))
-                    }
-                </tbody>
-            </table> */}
             <div className="container">
                 <div className="row">
                     {
-                        listaCategorias.map((categoria) => (
-
-                            <div className="col-4">
-                            <div className="card" style={{width: "18rem"}}>
-                                <img src={categoria.imagen} className="card-img-top" alt="hola" style={{width : "286" ,height : "171"} }></img>
-                                    <div className="card-body">
-                                        <h5 className="card-title">{categoria.nombre}</h5>
-                                        <p className="card-text">{categoria.descripcion}</p>
-                                        <a href="/categorias/form" className="btn btn-primary btn-sm me" >Editar</a>
-                                        {/* <button className="btn btn-primary btn-sm me" onclick={href="/categorias/form"}>Editar</button> */}
-                                    <button className="btn btn-danger btn-sm" onClick={() => {if(window.confirm('Esta seguro que desea eliminar esta categoria?')){ this.deleteHandler()};}}>Eliminar</button>
+                        (estado === Estados.CARGANDO) ?
+                            (
+                                // <tr>
+                                //     <td align="center" colSpan={4}>Cargando...</td>
+                                // </tr>
+                                <div className="alert alert-warning" role="alert" align="center" colSpan={4}>
+                                    Cargando...
+                                </div>
+                            ) :
+                            estado === Estados.VACIO ?
+                                (
+                                    // <tr>
+                                    //     <td align="center" colSpan={4}>No hay datos.</td>
+                                    // </tr>
+                                    <div className="alert alert-warning" role="alert" align="center" colSpan={4}>
+                                        No hay datos.
                                     </div>
-                            </div>
-                            </div>
-                        ))
+                                ) :
+                                estado === Estados.ERROR ?
+                                    (
+                                        // <tr>
+                                        //     <td align="center" colSpan={4}>Ocurrio un error. Por favor intente mas tarde</td>
+                                        // </tr>
+                                        <div className="alert alert-warning" role="alert" align="center" colSpan={4}>
+                                            Ocurrio un error. Por favor intente mas tarde
+                                        </div>
+                                    ) :
+                                    listaCategorias.map((categoria) => (
+
+                                        <div className="col-4">
+                                            <div className="card" style={{ width: "18rem" }}>
+                                                <img src={categoria.imagen} className="card-img-top" alt="hola" style={{ width: "286", height: "171" }}></img>
+                                                <div className="card-body">
+                                                    <h5 className="card-title">{categoria.nombre}</h5>
+                                                    <p className="card-text">{categoria.descripcion}</p>
+                                                    <a href="/categorias/form" className="btn btn-primary btn-sm me" >Editar</a>
+                                                    {/* <button className="btn btn-primary btn-sm me" onclick={href="/categorias/form"}>Editar</button> */}
+                                                    <button className="btn btn-danger btn-sm" onClick={() => { if (window.confirm('Esta seguro que desea eliminar esta categoria?')) { this.deleteHandler() }; }}>Eliminar</button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    ))
 
                     }
-
-
-
-
-
 
                 </div>
             </div>
--- src/componentes/general/Header.js
+++ src/componentes/general/Header.js
@@ -1,9 +1,10 @@
+// import icon from '../../imgs/*';
 const Header = () => {
     return (
         <div className="container">
             <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
                 <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-                    {/* <svg className="bi me-2" width="40" height="32"><use xlink:href="#bootstrap" /></svg> */}
+                    {/* <svg className="bi me-2" width="40" height="32"><img href={icon} /></svg> */}
                     <span className="fs-4">Noel Dulceria Online</span>
                 </a>
 
