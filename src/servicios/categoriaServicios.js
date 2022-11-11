0c8933a901e4a09481fe65d6d29bcfc8b8833621
annie-rojas <annie.rojas.serna@gmail.com>
2022-11-11 07:35:54 -0500

    Configurando proyecto para heroku

--- src/servicios/categoriaServicios.js
+++ src/servicios/categoriaServicios.js
@@ -1,30 +1,31 @@
 import axios from "axios";
 
+const URI = "https://noel-api.herokuapp.com/";
 
 const categoriaServicios = {};
 
 
 categoriaServicios.obtenerCategorias = () => {
-    return axios.get("http://localhost:8000/categorias");
+    return axios.get(URI+"categorias");
 };
 
 categoriaServicios.cargarCategorias = (id) => {
-    return axios.get("http://localhost:8000/categorias/"+id );
+    return axios.get(URI+"categorias/"+id );
 };
 
 categoriaServicios.buscarCategoriasPorCriterios = (criterio) => {
-    return axios.get("http://localhost:8000/categorias?q=" + criterio);
+    return axios.get(URI+"categorias?q=" + criterio);
 };
 
 categoriaServicios.guardarCategoria = (categoria) => {
-    return axios.post("http://localhost:8000/categorias", categoria);
+    return axios.post(URI+"categorias", categoria);
 };
 
 categoriaServicios.editarCategoria = (id,categoria) => {
-    return axios.put("http://localhost:8000/categorias/"+id, categoria);
+    return axios.put(URI+"categorias/"+id, categoria);
 };
 
 categoriaServicios.EliminarCategoria = (id,categoria) => {
-    return axios.delete("http://localhost:8000/categorias/"+id, categoria);
+    return axios.delete(URI+"categorias/"+id, categoria);
 };
 export default categoriaServicios;
