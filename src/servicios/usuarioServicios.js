9ff444e6e0f29bd87006c8f656eb664684bde6d0
Ricardo Lizcano <annie.rojas.serna@gmail.com>
2022-11-15 00:18:47 -0500

    Modificacion Heroku

--- src/servicios/usuarioServicios.js
+++ src/servicios/usuarioServicios.js
@@ -1,30 +1,31 @@
 import axios from "axios";
 
+const URI = "https://noel-api.herokuapp.com/";
 
 const usuarioServicios = {};
 
 
 usuarioServicios.obtenerUsuario = () => {
-    return axios.get("http://localhost:8000/usuarios");
+    return axios.get(URI+"usuarios");
 };
 
 usuarioServicios.cargarUsuario = (id) => {
-    return axios.get("http://localhost:8000/usuarios/"+id );
+    return axios.get(URI+"usuarios/"+id );
 };
 
 usuarioServicios.buscarUsuarioPorCriterios = (criterio) => {
-    return axios.get("http://localhost:8000/usuarios?q=" + criterio);
+    return axios.get(URI+"usuarios?q=" + criterio);
 };
 
 usuarioServicios.guardarUsuario= (usuario) => {
-    return axios.post("http://localhost:8000/usuarios", usuario);
+    return axios.post(URI+"usuarios", usuario);
 };
 
 usuarioServicios.editarUsuario= (id,usuario) => {
-    return axios.put("http://localhost:8000/usuarios/"+id, usuario);
+    return axios.put(URI+"usuarios/"+id, usuario);
 };
 
 usuarioServicios.EliminarUsuario= (id,usuario) => {
-    return axios.delete("http://localhost:8000/usuarios/"+id, usuario);
+    return axios.delete(URI+"usuarios/"+id, usuario);
 };
 export default usuarioServicios;
